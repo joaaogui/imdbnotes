31cea55fd0ef21c3c148ffcf2f1639efb89a4b4b
João Guilherme <joaaogui@gmail.com>
2018-12-19 20:05:39 -0200

    Caham

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" height="300" width="300">
+    <HelloWorld msg="Digite o nome da série"/>
   </div>
 </template>
 
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,72 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+    <div class="hello">
+        <h1>{{ msg }}</h1>
+        <input v-model="message" @keyup.enter="say" placeholder="Me edite">
+        <p>{{ info }}</p>
+    </div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+    import axios from 'axios';
+    import 'lodash';
+
+    export default {
+        name: 'HelloWorld',
+        props: {
+            msg: String
+        },
+        data: function () {
+            return {
+                message: "",
+                info: ""
+            }
+        },
+        methods: {
+            say: function (x) {
+                let url = `http://www.omdbapi.com/?t=${this.message}&apikey=5799c6fa`
+                axios
+                    .get(url)
+                    .then(response => {
+                        this.info = response.data.totalSeasons
+                        for (const seasonNumber of _.range(1, parseInt(response.data.totalSeasons) + 1)) {
+                            let url = `http://www.omdbapi.com/?t=${this.message}&Season=${seasonNumber}&apikey=5799c6fa`
+                            axios
+                                .get(url)
+                                .then(response => {
+                                    var sum = 0
+                                    for (const episode in response.data.Episodes) {
+                                        sum += parseFloat(response.data.Episodes[episode].imdbRating)
+                                        // console.log(sum)
+                                    }
+                                    
+                                    console.log("Season: " + seasonNumber + " Nota: " + (sum/parseInt(response.data.Episodes.length)).toFixed(2))
+                                })
+                        }
+                    })
+                    .catch(error => (this.info = "Série não encontrada"))
+
+            }
+        }
+    }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+    h3 {
+        margin: 40px 0 0;
+    }
+
+    ul {
+        list-style-type: none;
+        padding: 0;
+    }
+
+    li {
+        display: inline-block;
+        margin: 0 10px;
+    }
+
+    a {
+        color: #42b983;
+    }
 </style>
