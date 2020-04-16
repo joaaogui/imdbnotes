beeb84286486b2ddafee97c3d2164137603c332b
João Guilherme <joaaogui@gmail.com>
2020-04-16 18:13:31 -0300

    Updating project and changing get logic

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  'extends': [
+    'plugin:vue/strongly-recommended',
+    '@vue/standard'
+  ],
+  rules: {
+    'indent': ['off', 2],
+    'vue/require-default-prop': 'off',
+    'no-unused-vars': 'warn',
+    'no-new': 'off',
+    'vue/mustache-interpolation-spacing': 'off',
+    'vue/no-unused-components': 'warn',
+    'no-multiple-empty-lines': 'warn',
+    'vue/html-closing-bracket-spacing': 'off',
+    'space-before-function-paren': 'off',
+    'object-curly-spacing': 'off',
+    'vue/singleline-html-element-content-newline': 'off',
+    'quotes': 'warn',
+    'vue/valid-v-for': 'warn',
+    'comma-dangle': 'warn',
+
+  },
+  parserOptions: {
+    parser: 'babel-eslint'
+  }
+}
