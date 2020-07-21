b9a97cb057df29fc7b4912bc93f5d360061a0095
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-07-21 16:34:30 +0000

    Deploy imdb.joaoguilherme.me to github.com/joaaogui/IMDb-best_season.git:gh-pages

---
+++ precache-manifest.2954cce9f9a154b99850e7c953ee6f36.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d3c9f314c36801be5a0",
+    "url": "/css/chunk-0cba7658.1519c902.css"
+  },
+  {
+    "revision": "d38f7fa11ab08cb77266",
+    "url": "/css/chunk-e5f6cc6c.8f4e377c.css"
+  },
+  {
+    "revision": "4384f74eaa9a2e23b7f4",
+    "url": "/css/chunk-vendors.e5dc3084.css"
+  },
+  {
+    "revision": "6f384e9634c450468a8de16f88676661",
+    "url": "/img/logo.6f384e96.png"
+  },
+  {
+    "revision": "d600557066715bcaca7b268fc55939a4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "23ae147ebd3edad295cb",
+    "url": "/js/app.3e958105.js"
+  },
+  {
+    "revision": "0d3c9f314c36801be5a0",
+    "url": "/js/chunk-0cba7658.50774f68.js"
+  },
+  {
+    "revision": "330a49ccb3d9528eafe6",
+    "url": "/js/chunk-2d22cdfc.721aa614.js"
+  },
+  {
+    "revision": "d38f7fa11ab08cb77266",
+    "url": "/js/chunk-e5f6cc6c.bd3fd597.js"
+  },
+  {
+    "revision": "4384f74eaa9a2e23b7f4",
+    "url": "/js/chunk-vendors.2c10bc73.js"
+  },
+  {
+    "revision": "a2cb88ded4539119f2361c649ee868c0",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
